be27ec05609209cc66bf71b7efecf61cf43451dc
shubham <8shubhamrana8@gmail.com>
2026-02-21 21:48:45 +0530

    finalizing all

--- project.py
+++ project.py
@@ -16,66 +16,251 @@ def pathcheck():
             df = pd.read_csv(filepath)
             return df, filepath
         else:
-            print("No such file at diectory")
+            print("⚠️⚠️⚠️  No such file at diectory  ⚠️⚠️⚠️")
+
+
+
+def check_valid_input(prompt, input_type, condition = None, error_msg = "\n⚠️⚠️⚠️ INVALID INPUT ⚠️⚠️⚠️\n" ):
+    while True:
+        try:
+            value = input_type(input(prompt))
+            if condition:
+                if not condition(value):
+                    print(error_msg)
+                    continue
+            return value
+        except ValueError:
+            print(f"⚠️⚠️⚠️   Please enter a valid {input_type.__name__}.  ⚠️⚠️⚠️")
+
+
+
+def check_valid_date(year, month, day):
+    try:
+        datetime(int(year), int(month), int(day))
+        return True
+    except ValueError:
+        return False
+
+
+
+def getpercentage(value, total):
+    if total == 0:
+        return 0.0
+    return round((value / total) * 100, 2)
+
+
+
+def no_duplicate(datalist):
+    outputlist = []
+    for data in datalist:
+        if data in outputlist:
+            pass
+        else:
+            outputlist.append(data)
+    return sorted(outputlist)
+
 
 
 def menu(filepath, df):
 
     while True:
-        option = input(
+        option = check_valid_input(
             """
                 <<<<<<  MENU  >>>>>>
+
                 (1) Add Expense
+                
                 (2) View Summary
+                
                 (3) History
+                
                 (4) Check Budget Overrun
+                
                 (5) Exit
 
-                Choose an option: """
+                Choose an option: """, str
         )
+        option.strip().lower()
         print(f"Option {option} choosen")
-        if option == "1":
+        if option == "1" or option == "add expense":
+            print(f"\n✅✅✅ Add Expense to your CSV file ✅✅✅\n")
             df = addexpense(filepath, df)
 
-        elif option == "2":
+        elif option == "2" or option == "summary":
+            print(f"\n✅✅✅ Loading Summary ✅✅✅\n")
             summary(df)
             show_monthly_expenses(df)
             
 
-        elif option == "3":
+        elif option == "3" or option == "history" :
+            print(f"\n✅✅✅ Loading History ✅✅✅\n")
             history(df)
 
-        elif option == "4":
+        elif option == "4" or option == "check budget overrun":
+            print(f"\n✅✅✅ Loading Budget Overrun ✅✅✅\n")
             budgetoverrun(df)
 
-        elif option == "5":
-            print("Exiting")
+        elif option == "5" or option == "exit":
+            print("\nExiting!!! Thank You for using this programme!!!!!\n")
             sys.exit()
         else:
-            print("Enter Valid input in digits. Thank You!", end="\n")
+            print("⚠️⚠️⚠️   Enter Valid input in digits. Thank You!  ⚠️⚠️⚠️\n")
+
+
+
+def getexpense(df):
+
+    while True:
+        year = check_valid_input(prompt="\nEnter Year: ",
+                                input_type=int,
+                                condition=lambda x: x>0,
+                                error_msg="⚠️⚠️⚠️  Invalid Year  ⚠️⚠️⚠️")
+        month = check_valid_input(prompt="\nEnter Month: ",
+                                input_type=int,
+                                condition=lambda x: 0<x<13,
+                                error_msg="⚠️⚠️⚠️  Please check the month (1-12)  ⚠️⚠️⚠️")
+        day = check_valid_input(prompt="\nEnter Day: ",
+                                input_type=int,
+                                condition=lambda x: 0<x<32,
+                                error_msg="⚠️⚠️⚠️  Please check the days (1-31)  ⚠️⚠️⚠️")
+        if check_valid_date(year, month, day):
+            date = f"{year}-{month:02}-{day:02}"
+            break
+        else:
+            print("⚠️⚠️⚠️   Invalid Date! Please check the month (1-12) and days (1-31).   ⚠️⚠️⚠️ \n")
+
+
+    print("\n" * 2)
+    
+    amount = check_valid_input("Enter amount : ", float, lambda x: x > 0)
+
+    print("\n" * 2)
+
+    all_categories = no_duplicate(df["Category"].tolist())
+    category_dict = {i: category for i, category in enumerate(all_categories, start=1)}
+    category_keys = list(category_dict.keys())
+    print("Select Category : \n")
+
+    for num, category in category_dict.items():
+        print(f"({num})--> {category}")
+    print("\n" * 2)
+
+
+     
+    usr_cat_num = check_valid_input(
+        prompt="Select Category : ", 
+        input_type=int,
+        condition = lambda x: x in category_dict,
+        error_msg = "\n⚠️⚠️⚠️   Invalid choice. Try again.   ⚠️⚠️⚠️\n"
+        )
+
+    category = category_dict[usr_cat_num]
+
+
+    new_expense_format = {"Date": date, "Amount": amount, "Category": category}
+
+    return new_expense_format
+
+
+
+def addexpense(filename, df):
+
+    user_data = getexpense(df)
+    new_data_df = pd.DataFrame([user_data])
+    df = pd.concat([df, new_data_df], ignore_index=True)
+    df.to_csv(filename, mode="w", index=False)
+    print("\n✅✅✅   File updated successfully   ✅✅✅\n")
+    return df
+
+
+
+def summary(df):
+
+    if df.empty:
+        print("\n⚠️⚠️⚠️ CSV EMPTY  ⚠️⚠️⚠️\n")
+        return
+
+    total = df["Amount"].sum()
+    print(f"\n TOTAL SPENDING = ${total:,.2f}\n")
+    categoryspending = df.groupby("Category", as_index=False)["Amount"].sum()
+    categoryspending = categoryspending.sort_values(by="Amount", ascending=False)
+    categoryspending["Percentage"] = getpercentage(categoryspending["Amount"], total)
+    print(categoryspending.to_markdown(index=False))
+
+    categoryspending = categoryspending.set_index("Category")
+    
+    
+    plt.style.use('dark_background')
+    labels = categoryspending.index
+    sizes = categoryspending["Percentage"]
+
+    plt.figure(figsize=(7, 7))
+    plt.pie(
+        sizes,
+        labels=labels,
+        autopct="%1.1f%%",
+        startangle=90,
+        colors=plt.cm.Paired.colors,
+        
+    )
+    plt.title(f"Category Distribution (TOTAL: ${total:,.2f})")
+    plt.axis("equal")
+    
+    plt.show()
+
+
+
+def show_monthly_expenses(df):
+    copydf = df.copy()
+    copydf["Date"] = pd.to_datetime(copydf["Date"])
+    copydf["Year-Month"] = copydf["Date"].dt.strftime("%Y-%m")
+    copydf["Day"] = copydf["Date"].dt.strftime("%d")
+
+    unq_date = no_duplicate(copydf["Year-Month"].tolist())
+
+ 
+    usr_date = check_valid_input(
+                prompt="\n Enter year and month (YYYY-MM) : ",
+                input_type= str,
+                condition= lambda x: x in unq_date,
+                error_msg="⚠️⚠️⚠️  Date Not Found! Please choose from the list above.  ⚠️⚠️⚠️"
+                                    )
+
+    data = copydf[(copydf["Year-Month"] == usr_date)]
+    data = data[["Day", "Category", "Amount"]]
+    data = data.set_index("Day")
+    print(data.to_markdown())
+
 
 
 def budgetoverrun(df):
 
     budget_dict = {}
     categories = no_duplicate(df["Category"].to_list())
+    print("\n"*3)
     for category in categories:
-        budget = float(input(f"Set Budget for {category}--> $"))
+        budget = check_valid_input( prompt=f"Set Budget for {category}--> $", 
+                                    input_type = float, 
+                                    condition=lambda x : x > 0,
+                                    error_msg=f"⚠️⚠️⚠️INVALID INPUT!! Setting budget for {category} to $0.00⚠️⚠️⚠️"
+                                    )
         budget_dict[category] = budget
+
 
     category_spending = df.groupby("Category", as_index=False)["Amount"].sum()
     category_spending["Budget"] = category_spending["Category"].map(budget_dict)
-    category_spending["Overrun"] = (
-        category_spending["Amount"] - category_spending["Budget"]
-    )
+    category_spending["Overrun"] = (category_spending["Amount"] - category_spending["Budget"])
+    
     over_limit = category_spending[category_spending["Overrun"] > 0]
 
     if not over_limit.empty:
-        print("\n⚠️ Alert: You exceeded your budget in these categories:")
-        print(over_limit[["Category", "Overrun"]])
+        print("\n⚠️⚠️⚠️ Alert: You exceeded your budget in these categories: ")
+        
+        print("\n"*2,over_limit[["Category", "Overrun"]].to_markdown(index= False))
     else:
-        print("\n✅ Great job! You are within budget for all categories.")
-
+        print("\n✅✅✅  Great job! You are within budget for all categories.  ✅✅✅")
+    
+    plt.style.use('dark_background')
     labels = category_spending["Category"]
     actual = category_spending["Amount"]
     budget = category_spending["Budget"]
@@ -105,148 +290,14 @@ def budgetoverrun(df):
             )
 
     plt.tight_layout()
-    plt.savefig("budget_analysis.png")
     plt.show()
 
-
-def check_valid_date(year, month, day):
-    try:
-        datetime(int(year), int(month), int(day))
-        return True
-    except ValueError:
-        return False
-
-
-def getexpense(df):
-
-    while True:
-        year = input("Enter Year: ")
-        month = input("Enter Month: ")
-        day = input("Enter Day: ")
-        if check_valid_date(year, month, day):
-            date = year + "-" + month + "-" + day
-            break
-        else:
-            print("Enter valid date \n")
-
-    print("\n" * 2)
-
-    amount = float(input("Enter amount : "))
-
-    print("\n" * 2)
-
-    all_categories = df["Category"].unique()
-    cat_dict = {i: category for i, category in enumerate(all_categories, start=1)}
-
-    print("Select Category : \n")
-
-    for num, category in cat_dict.items():
-        print(f"({num})--> {category}")
-    print("\n" * 2)
-
-    while True:
-        usr_cat_num = int(input("Select Category : "))
-        if usr_cat_num in cat_dict:
-            category = cat_dict[usr_cat_num]
-            break
-        else:
-            print("Invalid choice. Try again.")
-
-    new_expense_format = {"Date": date, "Amount": amount, "Category": category}
-
-    return new_expense_format
-
-
-def getpercentage(value, total):
-    if total == 0:
-        return 0.0
-    return round((value / total) * 100, 2)
-
-
-def addexpense(filename, df):
-
-    user_data = getexpense(df)
-    new_data_df = pd.DataFrame([user_data])
-    df = pd.concat([df, new_data_df], ignore_index=True)
-    df.to_csv(filename, mode="w", index=False)
-    print("\n File updated successfully \n")
-    return df
-
-
-def summary(df):
-    month_map = {
-        "January": "01",
-        "February": "02",
-        "March": "03",
-        "April": "04",
-        "May": "05",
-        "June": "06",
-        "July": "07",
-        "August": "08",
-        "September": "09",
-        "October": "10",
-        "November": "11",
-        "December": "12",
-    }
-    total = df["Amount"].sum()
-    categoryspending = df.groupby("Category", as_index=False)["Amount"].sum()
-    categoryspending = categoryspending.sort_values(by="Amount", ascending=False)
-    categoryspending["Percentage"] = getpercentage(categoryspending["Amount"], total)
-    print(categoryspending.to_markdown())
-
-    categoryspending = categoryspending.set_index("Category")
-    labels = categoryspending.index
-
-    sizes = categoryspending["Percentage"]
-
-    plt.figure(figsize=(7, 7))
-    plt.pie(
-        sizes,
-        labels=labels,
-        autopct="%1.1f%%",
-        startangle=90,
-        colors=plt.cm.Paired.colors,
-    )
-    plt.title("Category Distribution")
-    plt.axis("equal")
-    plt.show()
-
-
-def no_duplicate(datalist):
-    outputlist = []
-    for data in datalist:
-        if data in outputlist:
-            pass
-        else:
-            outputlist.append(data)
-    return sorted(outputlist)
-
-
-def show_monthly_expenses(df):
-    copydf = df.copy()
-    copydf["Date"] = pd.to_datetime(copydf["Date"])
-    copydf["Year-Month"] = copydf["Date"].dt.strftime("%Y-%m")
-    copydf["Day"] = copydf["Date"].dt.strftime("%d")
-
-    unq_date = no_duplicate(copydf["Year-Month"].tolist())
-
-    while True:
-        usr_date = input("\n Enter year and month (YYYY-MM) : ")
-        if usr_date in unq_date:
-            data = copydf[(copydf["Year-Month"] == usr_date)]
-            data = data[["Day", "Category", "Amount"]]
-            data = data.set_index("Day")
-            print(data.to_string())
-            break
-        else:
-            print("\n Date Not Found")
 
 
 def history(df):
 
     while True:
-        choice = (
-            input(
+        choice = check_valid_input(
                 """
                         
                         Choose an option:
@@ -257,14 +308,19 @@ def history(df):
 
                         (C) Exit History
                         
-                        Enter Choice ----> """
+                        Enter Choice ----> """,
+                        str
             )
-            .strip()
-            .lower()
-        )
+        choice = choice.strip().lower()
+        data_years = no_duplicate(df["Date"].dt.year.tolist())
 
         if choice == "a":
-            yr = int(input("Enter Year : "))
+            yr = check_valid_input(
+                prompt="Enter Year : ",
+                input_type=int,
+                condition=lambda x: x in data_years,
+                error_msg="\n ⚠️⚠️⚠️  Invalid Year ⚠️⚠️⚠️"
+                )
             print("\n" * 2)
             print(f"Filtering Year {yr}", "\n" * 3)
             try:
@@ -274,18 +330,22 @@ def history(df):
                 monthly_expense = (
                     filterdf.groupby("Month")["Amount"].sum().reset_index()
                 )
-                me = monthly_expense.plot.bar(x="Month", y="Amount", rot=0)
+                if monthly_expense.empty:
+                    return
+                plt.style.use('dark_background')
+                monthly_expense.plot.bar(x="Month", y="Amount", rot=0)
+                print(f"\n Showing monthly spending of year {yr} \n")
                 plt.show()
                 break
             except (ValueError, IndexError):
-                print(f"{yr} Year not found", "\n" * 3)
+                print(f"⚠️⚠️⚠️   Year {yr} not found   ⚠️⚠️⚠️", "\n" * 3)
                 continue
 
         if choice == "b":
             df["Date"] = pd.to_datetime(df["Date"])
             df["Year"] = df["Date"].dt.to_period("Y").dt.year
             yearly_expense = df.groupby("Year")["Amount"].sum().reset_index()
-            ye = yearly_expense.plot.bar(x="Year", y="Amount", rot=0)
+            yearly_expense.plot.bar(x="Year", y="Amount", rot=0)
             plt.show()
             break
 
@@ -293,7 +353,8 @@ def history(df):
             break
 
         else:
-            print("Kindly enter A , B , C. Thank You!!")
+            print("\n⚠️⚠️⚠️    Kindly enter A , B , C. Thank You!!   ⚠️⚠️⚠️\n")
+
 
 
 def main():
